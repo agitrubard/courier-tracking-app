3733bdb7a4296229b83ceaf287e809dd8cb67b96
agitrubard <demiragitrubar@gmail.com>
2025-03-14 12:06:34 +0300

    #8 | `StoreEntity` Has Been Extended from `AbstractEntity`

--- src/main/java/dev/agitrubard/couriertracking/model/entity/StoreEntity.java
+++ src/main/java/dev/agitrubard/couriertracking/model/entity/StoreEntity.java
@@ -5,20 +5,17 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Document(collection = "stores")
-public class StoreEntity {
+public class StoreEntity extends AbstractEntity {
 
     @Id
     private UUID id;
     private String name;
     private Double latitude;
     private Double longitude;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
 }
