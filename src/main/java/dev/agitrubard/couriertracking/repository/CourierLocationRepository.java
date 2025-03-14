521c3792a9acbbaa175c74d5bf7d93af988c0795
agitrubard <demiragitrubar@gmail.com>
2025-03-14 19:37:59 +0300

    #8 | `findTopByCourierIdOrderByCreatedAtDesc()` Method Has Been Added to `CourierLocationRepository`

--- src/main/java/dev/agitrubard/couriertracking/repository/CourierLocationRepository.java
+++ src/main/java/dev/agitrubard/couriertracking/repository/CourierLocationRepository.java
@@ -3,7 +3,11 @@ package dev.agitrubard.couriertracking.repository;
 import dev.agitrubard.couriertracking.model.entity.CourierLocationEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CourierLocationRepository extends MongoRepository<CourierLocationEntity, UUID> {
+
+    Optional<CourierLocationEntity> findTopByCourierIdOrderByCreatedAtDesc(UUID courierId);
+
 }
