6f10fa0d7dbf20732672bf164a02e2a50a26c320
agitrubard <demiragitrubar@gmail.com>
2025-03-14 19:41:59 +0300

    #8 | `CourierLocationReadPort` Has Been Created and `findLastByCourierId()` Method Has Been Implemented in `CourierLocationAdapter`

--- src/main/java/dev/agitrubard/couriertracking/port/adapter/CourierLocationAdapter.java
+++ src/main/java/dev/agitrubard/couriertracking/port/adapter/CourierLocationAdapter.java
@@ -2,19 +2,32 @@ package dev.agitrubard.couriertracking.port.adapter;
 
 import dev.agitrubard.couriertracking.model.CourierLocation;
 import dev.agitrubard.couriertracking.model.entity.CourierLocationEntity;
+import dev.agitrubard.couriertracking.model.mapper.CourierLocationEntityToDomainMapper;
 import dev.agitrubard.couriertracking.model.mapper.CourierLocationToEntityMapper;
+import dev.agitrubard.couriertracking.port.CourierLocationReadPort;
 import dev.agitrubard.couriertracking.port.CourierLocationSavePort;
 import dev.agitrubard.couriertracking.repository.CourierLocationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Component
 @RequiredArgsConstructor
-class CourierLocationAdapter implements CourierLocationSavePort {
+class CourierLocationAdapter implements CourierLocationReadPort, CourierLocationSavePort {
 
     private final CourierLocationRepository courierLocationRepository;
 
     private final CourierLocationToEntityMapper courierLocationToEntityMapper = CourierLocationToEntityMapper.INSTANCE;
+    private final CourierLocationEntityToDomainMapper courierLocationEntityToDomainMapper = CourierLocationEntityToDomainMapper.INSTANCE;
+
+    @Override
+    public Optional<CourierLocation> findLastByCourierId(UUID courierId) {
+        return courierLocationRepository.findTopByCourierIdOrderByCreatedAtDesc(courierId)
+                .map(courierLocationEntityToDomainMapper::map);
+    }
+
 
     @Override
     public void save(final CourierLocation courierLocation) {
