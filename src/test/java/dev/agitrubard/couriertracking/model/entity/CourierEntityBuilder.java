7fa2e8796d10f865e08319cc014b0db1774bd53a
agitrubard <demiragitrubar@gmail.com>
2025-03-15 22:03:38 +0300

    #8 | `withCreatedAtNow()` Method Has Been Added to `CourierEntityBuilder`

--- src/test/java/dev/agitrubard/couriertracking/model/entity/CourierEntityBuilder.java
+++ src/test/java/dev/agitrubard/couriertracking/model/entity/CourierEntityBuilder.java
@@ -2,6 +2,7 @@ package dev.agitrubard.couriertracking.model.entity;
 
 import dev.agitrubard.couriertracking.model.TestDataBuilder;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class CourierEntityBuilder extends TestDataBuilder<CourierEntity> {
@@ -11,11 +12,17 @@ public class CourierEntityBuilder extends TestDataBuilder<CourierEntity> {
     }
 
     public CourierEntityBuilder withValidValues() {
-        return this;
+        return this
+                .withCreatedAtNow();
     }
 
     public CourierEntityBuilder withId(UUID id) {
         data.setId(id);
+        return this;
+    }
+
+    private CourierEntityBuilder withCreatedAtNow() {
+        data.setCreatedAt(LocalDateTime.now());
         return this;
     }
 
