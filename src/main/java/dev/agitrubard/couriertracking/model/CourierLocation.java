8a4740cdaaad0a44e68c79fd3584877dfea04a9e
agitrubard <demiragitrubar@gmail.com>
2025-03-15 20:24:27 +0300

    #8 | `CourierLocation` Class Has Been Refactored

--- src/main/java/dev/agitrubard/couriertracking/model/CourierLocation.java
+++ src/main/java/dev/agitrubard/couriertracking/model/CourierLocation.java
@@ -3,34 +3,18 @@ package dev.agitrubard.couriertracking.model;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 public class CourierLocation extends AbstractDomainModel {
 
     @Builder.Default
     private UUID id = UUID.randomUUID();
     private UUID courierId;
-    @Builder.Default
-    private Location location = Location.builder().build();
-
-
-    public static class CourierLocationBuilder {
-
-        private Location location;
-
-        public CourierLocationBuilder latitude(final Double latitude) {
-            this.location.setLatitude(latitude);
-            return this;
-        }
-
-        public CourierLocationBuilder longitude(final Double longitude) {
-            this.location.setLongitude(longitude);
-            return this;
-        }
-    }
+    private Location location;
 
 }
